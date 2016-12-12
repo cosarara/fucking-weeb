57aaeac754b960ebe271c801419ac902c3c09547
Jaume Delclòs Coll <cosa.rara97@gmail.com>
2016-12-12 23:40:17 +0100

    legalese

--- gtk3_bindings.h
+++ gtk3_bindings.h
@@ -1,24 +1,12 @@
 /*
- * This file was buit from parts of GTK, GDK, GLib and Pango
- * header files by Jaume Delclòs, 2016.
- * The following notices apply:
+ * This file is part of Fucking Weeb, and was buit from parts of
+ * GTK, GDK, GLib and Pango header files by Jaume Delclòs, 2016.
+ * Therefore, all the following notices apply:
  */
 
-/* GDK - The GIMP Drawing Kit
+/*
+ * GDK - The GIMP Drawing Kit
  * Copyright (C) 1995-1997 Peter Mattis, Spencer Kimball and Josh MacDonald
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -28,21 +16,9 @@
  * GTK+ at ftp://ftp.gtk.org/pub/gtk/.
  */
 
-/* GLIB - Library of useful routines for C programming
+/*
+ * GLIB - Library of useful routines for C programming
  * Copyright (C) 1995-1997  Peter Mattis, Spencer Kimball and Josh MacDonald
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -52,24 +28,29 @@
  * GLib at ftp://ftp.gtk.org/pub/gtk/.
  */
 
-/* Pango
- *
+/*
+ * Pango
  * Copyright (C) 1999 Red Hat Software
+ */
+
+/*
+ * Fucking Weeb
+ * Copyright © Jaume Delclòs Coll
+
+ * This file is part of Fucking Weeb.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * Fucking Weeb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * Fucking Weeb is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with Fucking Weeb.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 typedef enum
